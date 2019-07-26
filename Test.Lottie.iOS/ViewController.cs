38c14aa82d49bc5cce47311df065f573efeb4977
DoLT <dolt@runsystem.net>
2019-07-26 18:58:44 +0700

    fix test demo

--- Test.Lottie.iOS/ViewController.cs
+++ Test.Lottie.iOS/ViewController.cs
@@ -10,21 +10,33 @@ namespace Test.Lottie.iOS
         protected ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
+            this.View.BackgroundColor = UIColor.Black;
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+            var cAnimation = new CompatibleAnimation("splash", NSBundle.MainBundle);
 
-            // Perform any additional setup after loading the view, typically from a nib.
-            CompatibleAnimation compatibleAnimation = new CompatibleAnimation("splash", NSBundle.MainBundle);
-            CompatibleAnimationView animationView = new CompatibleAnimationView(new CoreGraphics.CGRect(0, 0, 200, 200))
+            var animView = new CompatibleAnimationView()
             {
-                CompatibleAnimation = compatibleAnimation
+                CompatibleAnimation = cAnimation,
             };
+            animView.LoopAnimationCount = -1; // Allow loop
+            animView.Frame = new CoreGraphics.CGRect(View.Frame.Width / 2, View.Frame.Height / 2, 200, 200);
+            animView.Center = View.ConvertPointFromView(View.Center, animView);
 
-            this.View.AddSubview(animationView);
+            animView.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            this.View.AddSubview(animView);
+
+            animView.TopAnchor.ConstraintEqualTo(View.TopAnchor).Active = true;
+
+            animView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor).Active = true;
+            animView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor).Active = true;
+            animView.BottomAnchor.ConstraintEqualTo(View.BottomAnchor).Active = true;
+            animView.Play();
         }
 
         public override void DidReceiveMemoryWarning()
